27c9b8c3b53a5da38416ff516ecd0156c6637818
N Sahana <162615011+Sahana128@users.noreply.github.com>
2025-03-09 16:01:50 +0530

    Add files via upload

---
+++ Project/Frontend/Src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Menu from "./Pages/Menu";
+import Contact from "./Pages/Contact";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar /> {/* ✅ Navbar now contains Login/Register links */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
